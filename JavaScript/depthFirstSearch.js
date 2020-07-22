3f291ba30a342619ee1829fcf27bcd211f7778bd
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2020-07-22 11:47:40 +0100

    depthFirstSearch

--- JavaScript/depthFirstSearch.js
+++ JavaScript/depthFirstSearch.js
@@ -1,7 +1,28 @@
-depthFirstSearch = (n, g) => {
+depthFirstSearch = (n, g, start) => {
     /* 
     n = numer of nodes in the graph
-    g = adjacency of the graph
+    g = adjacency of the graph e.g. [[1,2],[0,2],[0,1]] is a triangle
+    start = node to start search from
     */
-   
+   // creating an array of visited nodes
+    let visited = Array.apply(null, {length: (n)}).map(Boolean, false)
+    // begining the algorithm
+    let stack = []
+
+    visited[start] = true
+
+    function dfs(node) {
+        if(visited[node] === true) {return}
+        else{ 
+            visited[node] = true
+            stack.push(node)
+        }
+
+        let neightbourhood = g[node]
+        for (next in neightbourhood){
+            dfs(next)
+        }
+    }
+    console.log(stack)
+    return dfs(start)
 }
