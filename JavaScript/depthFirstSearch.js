ebb5b499ba3af9ea34dc77e1101846f58dcb0a58
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2020-07-22 10:54:28 +0100

    initial commit

---
+++ JavaScript/depthFirstSearch.js
@@ -0,0 +1,7 @@
+depthFirstSearch = (n, g) => {
+    /* 
+    n = numer of nodes in the graph
+    g = adjacency of the graph
+    */
+   
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # graph_theory_algorithms
+working on right algorithms for common graph theory problems in python and javascript
